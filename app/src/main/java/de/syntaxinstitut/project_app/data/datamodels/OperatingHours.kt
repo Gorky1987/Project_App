0f1ce56c564e5cae098bc502de097dec6812c117
Gorky1987 <103400152+Gorky1987@users.noreply.github.com>
2022-11-30 16:07:58 +0100

    No.5 = API CAll ui Design

--- app/src/main/java/de/syntaxinstitut/project_app/data/datamodels/OperatingHours.kt
+++ app/src/main/java/de/syntaxinstitut/project_app/data/datamodels/OperatingHours.kt
@@ -2,18 +2,28 @@ package de.syntaxinstitut.project_app.data.datamodels
 
 data class OperatingHours(
 
-    val mittwoch : String  = "",
-    val donnerstag : String  = "",
+    val mittwoch: String = "",
+    val donnerstag: String = "",
+    val freitag: String = "",
+    val samstag: String = "",
+    val sonntag: String = "",
+    val montag: String = "",
+    val dienstag: String = "",
 ) {
-    override fun toString ():String{
+    override fun toString(): String {
 
-       return """
+        return """
             Öffnungszeiten: 
             
+           Montag: $montag
+           Dienstag: $dienstag
            Mittwoch: $mittwoch
            Donnerstag: $donnerstag
-           
-           
+           Freitag: $freitag 
+           Samstag: $samstag
+           Sonntag: $sonntag
+
+
         """.trimIndent()
 
     }
